3732eec683a0c235c14ef1d6c86d04c714cf9ae3
TailsxKyuubi <Travianer88>
2020-02-02 11:14:33 +0100

    Added Coders wanted message

--- resources/views/partials/alerts.blade.php
+++ resources/views/partials/alerts.blade.php
@@ -5,6 +5,7 @@
       @if (config('other.freeleech') == true) @lang('common.freeleech_activated')! @endif
         @if (config('other.invite-only') == false) @lang('common.openreg_activated')! @endif
         @if (config('other.doubleup') == true) @lang('common.doubleup_activated')! @endif
+          <b> Developers wanted! Come help us make ACM awesome! More info <a href="https://asiancinema.me/forums/topic/coders-wanted.87" target="_blank">HERE</a></b>
     </span>
             <strong>
                 <div id="promotions"></div>
